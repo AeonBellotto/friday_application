0e209738795457c3e69756ba7bccbb0b8deb78b4
AeonBellotto <lor.bellotto@gmail.com>
2023-01-18 10:59:40 +0100

    console reader

--- lib/SCREENS/mid_side.dart
+++ lib/SCREENS/mid_side.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:dart_console/dart_console.dart';
 
 class MidSide extends StatefulWidget {
   const MidSide({super.key});
@@ -10,10 +11,37 @@ class MidSide extends StatefulWidget {
 }
 
 class _MidSideState extends State<MidSide> {
+  String _consoleLine = "";
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _consoleLine = sshCom();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(),
+      child: Container(
+        child: Text(
+          _consoleLine,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
+}
+
+String sshCom() {
+  final console = Console();
+  String line = "";
+
+  console.clearScreen();
+  console.resetCursorPosition();
+
+  line = console.readLine()!;
+
+  return line;
 }
